e385e674233b7da49524b6b15b7d2129ec22a8cb
rfranca86 <universius@gmail.com>
2017-10-07 00:33:30 -0300

    setup da gem Fae

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  require 'sidekiq/web'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+require 'sidekiq/web'
   
-  Rails.application.routes.draw do
-    mount Sidekiq::Web => '/sidekiq'
-  end
+Rails.application.routes.draw do
+
+namespace :admin do
+end
+# mount Fae below your admin namespec
+mount Fae::Engine => '/admin'
+
+  mount Sidekiq::Web => '/sidekiq'
 end
