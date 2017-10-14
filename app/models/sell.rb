52012a4dae322a964742c5d96931a9a4cecbc016
rfranca86 <universius@gmail.com>
2017-10-14 01:31:37 -0300

    customizing admin

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,11 +1,17 @@
 class Sell < ApplicationRecord
-  include Fae::BaseModelConcern
+include Fae::BaseModelConcern
+enum status: { finished: 0, canceled: 1 }
+	
+validates :client, presence: true
 
-  def fae_display_field
-    
-  end
+def fae_display_field
+  id
+end
 
+def self.for_fae_index
+  order(:id)
+end
 
-  belongs_to :discount
-  belongs_to :client
+belongs_to :discount
+belongs_to :client
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,6 +1,16 @@
 class Supplier < ApplicationRecord
   include Fae::BaseModelConcern
 
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :email,
+    uniqueness: true, # nao e unico no sistema todo, pode ter um client e um supplier com mesmo email
+    presence: true,
+    format: {
+      with: Fae.validation_helpers.email_regex,
+      message: 'You need use a valid and unique email'
+    }
+
   def fae_display_field
     name
   end
