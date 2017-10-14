8e7357fd3c253835f58d6e95114604e7066f6dd9
rfranca86 <universius@gmail.com>
2017-10-14 01:54:35 -0300

    setting many through and setting self.name for list sells

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,17 +1,24 @@
 class Sell < ApplicationRecord
-include Fae::BaseModelConcern
-enum status: { finished: 0, canceled: 1 }
-	
-validates :client, presence: true
+  include Fae::BaseModelConcern
+  enum status: { finished: 0, canceled: 1 }
+    
+  validates :client, presence: true
 
-def fae_display_field
-  id
-end
+  belongs_to :discount
+  belongs_to :client
 
-def self.for_fae_index
-  order(:id)
-end
+  has_many :sell_products
+  has_many :products, through: :sell_products
 
-belongs_to :discount
-belongs_to :client
+  has_many :sell_services
+  has_many :services, through: :sell_services
+
+  def fae_display_field
+    self.name
+  end
+
+  def self.for_fae_index
+    order(:id)
+  end
+
 end
